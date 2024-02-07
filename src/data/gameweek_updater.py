cef6d73e70bf4c624478011fed5843e08ca98f8a
Nathan Gootee <nathangootee@outlook.com>
2024-02-07 16:33:07 +1100

    add updater script and gameweek 23 update

---
+++ src/data/gameweek_updater.py
@@ -0,0 +1,15 @@
+import sys
+from get_fixture_data import get_fixture_data_func
+from get_player_data import get_player_data_func
+
+# odm dir
+sys.path.append("src/models")
+from odm import odm_func
+
+# read in finished gameweek from user input
+GW = int(sys.argv[1])
+
+# run data and model scripts
+get_fixture_data_func(GW)
+odm_func(GW + 1)
+get_player_data_func(GW)
