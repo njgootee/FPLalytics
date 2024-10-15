283aff7fb3a32f081ff8f7ae3d856005f5ab1238
Nathan Gootee <nathangootee@outlook.com>
2024-10-14 22:29:25 -0700

    points projections notice

--- src/streamlit/Home.py
+++ src/streamlit/Home.py
@@ -46,6 +46,8 @@ Use our latest data, stats, and models to prepare your team for success in Gamew
 # development updates
 st.markdown(
 """##### Development Updates
+    - Points Projections
+        - Live for 2024 season
     - Fixture Ticker
         - Home/Away fixtures
     - Player Comparison dashboard updates
